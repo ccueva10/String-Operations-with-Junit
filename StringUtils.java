0bd27cec980cf76c9e9db6c00ae05f81f21ea263
HDhami12 <158859540+HDhami12@users.noreply.github.com>
2024-04-16 20:42:43 -0700

    Update StringUtils.java

--- StringUtils.java
+++ StringUtils.java
@@ -5,7 +5,6 @@ public class StringUtils {
 
     public static String reverse(String str) {
         return new StringBuilder(str).reverse().toString();
-       
     }
 
     public static String toUpperCase(String str) {
