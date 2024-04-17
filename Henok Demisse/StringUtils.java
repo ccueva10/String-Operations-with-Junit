f8f5069c6135c6543073284ce492a28f629fe29e
henok1987 <88854040+henok1987@users.noreply.github.com>
2024-04-16 21:39:08 -0700

    Update StringUtils.java

--- Henok Demisse/StringUtils.java
+++ Henok Demisse/StringUtils.java
@@ -1,1 +1,42 @@
+public class StringUtils {
+
+    // Method for concatenation of two strings
+    public static String concatenate(String str1, String str2) {
+        if (str1 == null) {
+            str1 = "";
+        }
+        if (str2 == null) {
+            str2 = "";
+        }
+        return str1 + str2;
+    }
+
+    // Reversal of a string Method
+    public static String reverse(String str) {
+        if (str == null) {
+            return null;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    // Conversion of a string to uppercase Method
+    public static String toUpperCase(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.toUpperCase();
+    }
+
+    // Trimming of leading and trailing whitespace Method 
+    public static String trimWhitespace(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.trim();
+    }
+
+    // main goes here
+}
+
+
 
